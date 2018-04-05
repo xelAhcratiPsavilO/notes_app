c43af8b81179cdc12451c0068658a9c3a4a08551
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-04-05 13:43:50 +0100

    Refactor tests

--- tests/note-list-tests.js
+++ tests/note-list-tests.js
@@ -3,16 +3,16 @@ function testNoteList() {
   var notelist = new NoteList();
   var note1 = new Note('I am the 1st note');
   var note2 = new Note('I am the 2nd note');
-  
-  document.write('NL is initialized wih an empty array', expect(notelist.content).toBeEmpty());
+
+  document.write(' NL is initialized wih an empty array ', expect(notelist.content).toBeEmpty());
 
   notelist.adds(note1);
 
-  document.write('NL can add notes to its array', expect(notelist.content).toNotToBeEmpty());
+  document.write(' NL can add notes to its array ', expect(notelist.content).toNotToBeEmpty());
 
   notelist.adds(note2);
 
-  document.write('NL can show all the notes on its array', expect(notelist.show()[1]).toEqual(note2));
+  document.write(' NL can show all the notes on its array ', expect(notelist.show()[1]).toEqual(note2));
 };
 
 testNoteList();
--- tests/note-tests.js
+++ tests/note-tests.js
@@ -2,8 +2,8 @@
 function testNote() {
   var note = new Note('I am a new note');
 
-  document.write('N is initialized with text', expect(note.text).toEqual('I am a new note'));
-  document.write('N can show its text', expect(note.show()).toEqual('I am a new note'));
+  document.write(' N is initialized with text ', expect(note.text).toEqual('I am a new note'));
+  document.write(' N can show its text ', expect(note.show()).toEqual('I am a new note'));
 };
 
 testNote();
