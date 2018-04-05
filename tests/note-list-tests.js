3ea7d1139c331426a6b674c023debbfbd93f70fe
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-04-05 13:20:44 +0100

    Remove unnecessary console.log(s)

--- tests/note-list-tests.js
+++ tests/note-list-tests.js
@@ -3,15 +3,15 @@ function testNoteList() {
   var notelist = new NoteList();
   var note1 = new Note('I am the 1st note');
   var note2 = new Note('I am the 2nd note');
-  console.log(notelist.content);
+  
   document.write('NL is initialized wih an empty array', expect(notelist.content).toBeEmpty());
 
   notelist.adds(note1);
-  console.log(notelist.content);
+
   document.write('NL can add notes to its array', expect(notelist.content).toNotToBeEmpty());
 
   notelist.adds(note2);
-  console.log(notelist.content);
+
   document.write('NL can show all the notes on its array', expect(notelist.show()[1]).toEqual(note2));
 };
 
