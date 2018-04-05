5b857f8baf9671f854586a7db988776feccf9a40
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-04-05 17:25:42 +0100

    Add show function for NoteListView constructor function, green phase

--- src/note-list-view.js
+++ src/note-list-view.js
@@ -1,6 +1,17 @@
 (function(exports) {
-  function NoteListView() {
-    this.core = new NoteList;
+  function NoteListView(notelist) {
+    this.core = notelist;
+  };
+
+  NoteListView.prototype.show = function () {
+  var ultag = ['<ul>'];
+
+    this.core.show().forEach(function(note) {
+      ultag.push('<li><div>' + note.text + '</div></li>');
+    });
+
+    ultag.push('</ul>');
+    return ultag.join('');
   };
 
   exports.NoteListView = NoteListView
--- tests/note-list-view-tests.js
+++ tests/note-list-view-tests.js
@@ -1,8 +1,17 @@
 
 function testNoteListView() {
-  var notelistview = new NoteListView();
+  var notelist = new NoteList();
+  var note1 = new Note('Favourite food: paella');
+  var note2 = new Note('Favourite drink: aquarius');
+
+  notelist.adds(note1);
+  notelist.adds(note2);
+
+  var notelistview = new NoteListView(notelist);
 
   document.write(' NLV is initialized wih an NL ', expect(notelistview.core).toBe(NoteList));
+
+  document.write(' NLV can show the NL ', expect(notelistview.show()).toEqual('<ul><li><div>Favourite food: paella</div></li><li><div>Favourite drink: aquarius</div></li></ul>'));
 };
 
 testNoteListView();
